5d47b50232226fdbc9197f6bf16e99d7679ae814
student-Siddhi <siddhijaiswal53@gmail.com>
2025-03-02 08:37:10 +0530

    Time: 3 ms (45.14%), Space: 13.1 MB (86.43%) - LeetHub

---
+++ 1380-number-of-closed-islands/1380-number-of-closed-islands.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+      int m,n;
+
+      bool dfs(vector<vector<int>>& grid, int r, int c) {
+
+        if(r >= m || r <0 || c>=n || c<0)   //out of bound
+        return false;
+
+        if(grid[r][c] ==1) {   //water
+            return true;  //closed
+        }
+        grid[r][c] =1;  //mark visited;
+
+        bool left_closed = dfs(grid, r,c-1);
+         bool right_closed = dfs(grid, r,c+1);
+          bool up_closed = dfs(grid, r-1,c);
+           bool down_closed = dfs(grid, r+1,c);
+
+           return left_closed && right_closed && up_closed &&  down_closed;
+      }
+    int closedIsland(vector<vector<int>>& grid) {
+
+        m = grid.size();
+        n = grid[0].size();
+
+        int count = 0;
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+
+                if(grid[i][j] ==0) { // land
+                  if(dfs(grid, i, j) == true) {
+                    count ++;
+                  }
+                }
+            }
+        }
+        return count;
+    }
+};
